2ba2069ee82f6a9912c6e61d8c34f712b9312979
joeljose101 <76038274+joeljose101@users.noreply.github.com>
2026-03-18 09:33:50 +0000

    Deploying to gh-pages from @ joeljose101/joel-portfolio@4c3e133552d82580e68fcc0c9b1225b5296826fe 🚀

--- assets/Navbar-CJQQgpbB.js
+++ assets/Navbar-CJQQgpbB.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-BHoA5Y8h.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as P,r as bt}from"./index-Bj6jcaa9.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
